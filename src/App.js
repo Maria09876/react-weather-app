08000010565c2250c0aa37ef8ce62ed202097fa0
Maria09876 <maria.efthymiou46@gmail.com>
2022-08-17 15:30:01 +0200

    added github repository and personal website href

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        <a href='#' target='_blank'></a> 
+        This project was created by <a href='https://www.mariaefthymiou.com/' target='_blank'  rel="noreferrer">Maria Efthymiou</a>  and it is 
+        <a href='https://github.com/Maria09876/react-weather-app' target='_blank'  rel="noreferrer"> open sourced</a>. 
       </footer>
     </div>
   );
