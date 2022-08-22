50d06dfdfea0a756618443ac9bef56f4eb69eef0
Maria09876 <maria.efthymiou46@gmail.com>
2022-08-22 10:18:47 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-
+import React from "react"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
+      <button   className="btn btn-primary">hello</button>
       <footer>
-        This project was created by <a href='https://www.mariaefthymiou.com/' target='_blank'  rel="noreferrer">Maria Efthymiou</a>  and it is 
-        <a href='https://github.com/Maria09876/react-weather-app' target='_blank'  rel="noreferrer"> open sourced</a>. 
+        This project was created by <a href='https://www.mariaefthymiou.com/' target='_blank' rel="noopener noreferrer" >Maria Efthymiou</a>  and it is 
+        <a href='https://github.com/Maria09876/react-weather-app' target='_blank' rel="noopener noreferrer" > open sourced</a>. 
       </footer>
+      </div>
     </div>
   );
 }
